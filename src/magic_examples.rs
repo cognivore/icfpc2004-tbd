8f18dbbd72800f8cc82c997db09086a5ca5d4dde
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2020-09-07 06:16:50 +0300

    Add spiral ant example

--- src/magic_examples.rs
+++ src/magic_examples.rs
@@ -1,5 +1,28 @@
+/*
+Functions that directly or indirectly call ant IO (like move, sense)
+should return AntResult<T> instead of T.
+So AntResult is kind of like AntIO monad.
+
+Such functions should always be invoked using call!() macro,
+and not directly.
+
+All local variables should be wrapped in var!().
+Use v.get() and v.set() to access them.
+
+This is a hack so maybe there are some limitations that didn't occur to me.
+*/
+
 use crate::magic::*;
+use crate::neurology::LR;
 use crate::{call, var};
+
+fn move_n(n: i32) -> AntResult<()> {
+    for i in 0..n {
+        var!(let _i = i);
+        call!(move_());
+    }
+    Ok(())
+}
 
 fn looping_ant() -> AntResult<()> {
     if call!(flip(3)) {
@@ -8,15 +31,31 @@ fn looping_ant() -> AntResult<()> {
         call!(move_());
     }
     loop {
-        for i in 0..3 {
-            var!(let _i = i);
-            call!(move_());
-        }
+        call!(move_n(3));
+        call!(drop());
+    }
+}
+
+fn spiral_ant() -> AntResult<()> {
+    var!(let mut k = 1);
+
+    while k.get() <=4 {
+        call!(move_n(k.get()));
+        call!(turn(LR::Left));
+        k.set(k.get() * 2);
+    }
+
+    loop {
         call!(drop());
     }
 }
 
 // ENTRY_POINT
 pub fn magic_example() {
+    eprintln!("looping ant");
     eprintln!("{:?}", compile(looping_ant));
+    eprintln!();
+    eprintln!("spiral ant");
+    eprintln!("{:?}", compile(spiral_ant));
+    eprintln!();
 }
